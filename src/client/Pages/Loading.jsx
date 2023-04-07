6f5ccee0f1face6f6642d0c57d3ffbc636255cba
Linus Haren <linus@haren.io>
2023-04-07 22:00:17 +0200

    better responsiveness for smaller devices

--- src/client/Pages/Loading.jsx
+++ src/client/Pages/Loading.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styles from './Loading.module.css'
 import { SpinningCircles } from 'react-loading-icons'
 
 function Loading({loadingMessage}) {
+
+  useEffect(() => {
+
+  }, [loadingMessage])
+
   return (
     <div className={styles.container}>
       <SpinningCircles />
